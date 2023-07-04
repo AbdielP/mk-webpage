4e067c91b322a0bc0020d61ac54a937b6ad3d414
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-04 18:34:26 -0500

    navbar estilizado

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -5,15 +5,15 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 export default function Navbar() {
   return (
     <nav className='navbar'>
-      <a href="">Contacto</a>
       <div class="dropdown">
-        <button class="dropbtn">Servicios <ArrowDropDownIcon fontSize="medium"></ArrowDropDownIcon></button>
+        <button class="dropbtn"><ArrowDropDownIcon fontSize="medium"></ArrowDropDownIcon>Servicios</button>
         <div class="dropdown-content">
           <a href="#">Link 1</a>
           <a href="#">Link 2</a>
           <a href="#">Link 3</a>
         </div>
       </div>
+      <a href="">Contacto</a>
       <a href="">Ubicación</a>
     </nav>
   )
