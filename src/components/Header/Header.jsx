e841b19c66ab9bd6feaf8492a74d0071f3a09941
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-04 09:36:43 -0500

    header & variables

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderBackground from "./HeaderBackground";
+import Navbar from "./Navbar";
 import "../../styles/header.css";
 
 export default function Header() {
   return (
     <header className="header">
+      <Navbar />
       <HeaderBackground />
       <div className="header__div">
         <h1 className="h1">Taller <span className="span__m">M</span><span className="span__k">K</span></h1>
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/navbar.css'
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+      <a href="">Contacto</a>
+      <a href="">Servicios</a>
+      <a href="">Ubicación</a>
+    </nav>
+  )
+}
