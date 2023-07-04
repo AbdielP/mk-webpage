a201d1d16bb687a330ebfe8ea8019777de9da0c4
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-03 22:12:23 -0500

    header base

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,13 @@ import "../../styles/header.css";
 export default function Header() {
   return (
     <header className="header">
+      <HeaderBackground />
       <div className="header__div">
-        <h1 className="h1">Taller MK</h1>
-        <h2>Mecánica general avanzada </h2>
-        <h3>Taller en Panamá</h3>
+        <h1 className="h1">Taller <span className="span__m">M</span><span className="span__k">K</span></h1>
+        <h2 className="h2">Mecánica general avanzada</h2>
+        {/* <h3 className="h3">Taller en Panamá</h3> */}
       </div>
       
-      {/* <HeaderBackground /> */}
     </header>
   );
 }
