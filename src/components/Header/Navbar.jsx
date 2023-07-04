6b2e3de59294e5443a2ce6ee75c6ae01d20c2d00
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-04 17:13:27 -0500

    dropdown menu base listo

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import '../../styles/navbar.css'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 
 export default function Navbar() {
   return (
     <nav className='navbar'>
       <a href="">Contacto</a>
-      <a href="">Servicios</a>
+      <div class="dropdown">
+        <button class="dropbtn">Servicios <ArrowDropDownIcon fontSize="medium"></ArrowDropDownIcon></button>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
       <a href="">Ubicación</a>
     </nav>
   )
