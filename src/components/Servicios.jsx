6b84fc94755e33ebd14753a2086a20feedf4f56a
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-05 12:05:36 -0500

    parrafos servicios

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,9 +1,21 @@
-import '../styles/servicios.css'
+import "../styles/servicios.css";
 
 export default function Servicios() {
   return (
-    <section className='section__servicios'>
-      <h3 className='h3'>Nuestros Servicios</h3>
+    <section className="section__servicios">
+      <h3 className="h3">Nuestros Servicios</h3>
+      <div className="div__info">
+        <p className="p__servicios">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum vel
+          sint necessitatibus esse tempora velit provident corporis, asperiores
+          laborum earum.
+        </p>
+        <hr className="hr" />
+        <p className="p__info">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt,
+          magni?
+        </p>
+      </div>
     </section>
-  )
+  );
 }
