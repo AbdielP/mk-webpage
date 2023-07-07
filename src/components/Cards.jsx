26d68b7c59259999f6044bc8a14eb4ac1e623789
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-07 04:40:04 -0500

    tarjetas completas

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,27 +6,6 @@ import Button from '@mui/material/Button'
 export default function Cards() {
   return (
     <div className='container__cards'>
-      <div className='card'>
-        <div className="card__header">
-          <BuildCircleTwoToneIcon fontSize="large" sx={{ color: 'white'}} />
-          <p className='card__p'>SERVICIOS</p>
-        </div>
-        <div className='card__container'>
-          <p>Mantenimiento completo</p>
-          <p>Mecánica general</p>
-          <p>Mecánica avanzada</p>
-          <p>Servicio de recogida de vehículo</p>
-          {/* <ol>
-            <li>Mantenimiento completo</li>
-            <li>Mecánica general</li>
-            <li>Mecánica avanzada</li>
-            <li>Servicio de recogida de vehículo</li>
-          </ol> */}
-          <Button variant="outlined" color='info' endIcon={<ArrowForwardSharpIcon />} sx={{marginTop: '1em'}}>
-            Leer más
-          </Button>
-        </div>
-      </div>
 
       <div className='card'>
         <div className="card__header">
@@ -34,15 +13,18 @@ export default function Cards() {
           <p className='card__p'>SERVICIOS</p>
         </div>
         <div className='card__container'>
-          <p>Mantenimientos</p>
-          <p>Mecánica general</p>
-          <p>Mecánica avanzada</p>
-          <p>Servicio de grúa</p>
-          <Button variant="outlined" color='info' endIcon={<ArrowForwardSharpIcon color='secondary' />}>
+          <ol className='ol__servicios'>
+            <li>Mantenimiento completo</li>
+            <li>Mecánica general</li>
+            <li>Mecánica avanzada</li>
+            <li>Servicio de recogida de vehículo</li>
+          </ol>
+          <Button href='#' endIcon={<ArrowForwardSharpIcon />} sx={{ marginTop: '.3em', color: '#FA5858' }}>
             Leer más
           </Button>
         </div>
       </div>
+
     </div>
   )
 }
