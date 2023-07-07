93bb79cf5dba447af570a9d9bb0861d3160cbbdb
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-07 05:43:07 -0500

    height tarjetas

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import '../styles/cards.css'
 import BuildCircleTwoToneIcon from '@mui/icons-material/BuildCircleTwoTone'
 import ArrowForwardSharpIcon from '@mui/icons-material/ArrowForwardSharp'
+import AccessTimeTwoToneIcon from '@mui/icons-material/AccessTimeTwoTone'
 import Button from '@mui/material/Button'
 
 export default function Cards() {
@@ -19,9 +20,21 @@ export default function Cards() {
             <li>Mecánica avanzada</li>
             <li>Servicio de recogida de vehículo</li>
           </ol>
-          <Button href='#' endIcon={<ArrowForwardSharpIcon />} sx={{ marginTop: '.3em', color: '#FA5858' }}>
+          <Button href='#' endIcon={<ArrowForwardSharpIcon />} sx={{ color: '#FA5858' }}>
             Leer más
           </Button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className="card__header">
+          <AccessTimeTwoToneIcon fontSize="large" sx={{ color: 'white'}} />
+          <p className='card__p'>HORARIO</p>
+        </div>
+        <div className='card__container'>
+          <p>Lunes a Sábado: <span className='span__horario'>08:00 a.m - 5:00 p.m</span></p>
+          <p>Domingos: <b>Cerrado</b></p>
+          <p>Emergencias: <span>24 Horas / 7 Días</span></p>
         </div>
       </div>
 
