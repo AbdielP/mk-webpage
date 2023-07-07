b2be07deb6a64e06a00ec2d6766c3f2bac90616d
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-07 04:23:13 -0500

    forma base tarjetas

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,7 @@
 import '../styles/cards.css'
-import BuildCircleTwoToneIcon from '@mui/icons-material/BuildCircleTwoTone';
+import BuildCircleTwoToneIcon from '@mui/icons-material/BuildCircleTwoTone'
+import ArrowForwardSharpIcon from '@mui/icons-material/ArrowForwardSharp'
+import Button from '@mui/material/Button'
 
 export default function Cards() {
   return (
@@ -10,13 +12,35 @@ export default function Cards() {
           <p className='card__p'>SERVICIOS</p>
         </div>
         <div className='card__container'>
-          <ol className='ol__servicios'>
-            <li>Mantenimiento de vehiculos de cualquier marca</li>
+          <p>Mantenimiento completo</p>
+          <p>Mecánica general</p>
+          <p>Mecánica avanzada</p>
+          <p>Servicio de recogida de vehículo</p>
+          {/* <ol>
+            <li>Mantenimiento completo</li>
             <li>Mecánica general</li>
             <li>Mecánica avanzada</li>
-            <li>Servicio de grúa</li>
-          </ol>
-          {/* Botón */}
+            <li>Servicio de recogida de vehículo</li>
+          </ol> */}
+          <Button variant="outlined" color='info' endIcon={<ArrowForwardSharpIcon />} sx={{marginTop: '1em'}}>
+            Leer más
+          </Button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className="card__header">
+          <BuildCircleTwoToneIcon fontSize="large" sx={{ color: 'white'}} />
+          <p className='card__p'>SERVICIOS</p>
+        </div>
+        <div className='card__container'>
+          <p>Mantenimientos</p>
+          <p>Mecánica general</p>
+          <p>Mecánica avanzada</p>
+          <p>Servicio de grúa</p>
+          <Button variant="outlined" color='info' endIcon={<ArrowForwardSharpIcon color='secondary' />}>
+            Leer más
+          </Button>
         </div>
       </div>
     </div>
