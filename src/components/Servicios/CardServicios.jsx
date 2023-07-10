b41147b2a06968a8cd2449f9896b734de259e45e
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-10 03:15:42 -0500

    tarjetas de servicios. estilos basicos

--- src/components/Servicios/CardServicios.jsx
+++ src/components/Servicios/CardServicios.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import '../../styles/cardservicios.css'
+import engine from '../../assets/images/n55.jpg'
 
 export default function CardServicios() {
   return (
-    <div>CardServicios</div>
+    <>
+        <div className='card__services'>
+            <h4>NOMBRE SERVICIO</h4>
+            <img className='img__card_services' src={engine} alt="image" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit nostrum at reprehenderit neque fugit cum?</p>
+        </div>
+
+        <div className='card__services'>
+            <h4>NOMBRE SERVICIO</h4>
+            <img className='img__card_services' src={engine} alt="image" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit nostrum at reprehenderit neque fugit cum?</p>
+        </div>
+
+        <div className='card__services'>
+            <h4>NOMBRE SERVICIO</h4>
+            <img className='img__card_services' src={engine} alt="image" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit nostrum at reprehenderit neque fugit cum?</p>
+        </div>
+    </>
   )
 }
