8b138f17303a9a982343c2687febbb4d0a0660d3
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-12 01:30:03 -0500

    tarjeta ubicacion e imagenes

--- src/components/Ubicacion.jsx
+++ src/components/Ubicacion.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import '../styles/ubicacion.css'
+import test from '../assets/images/local_foto.jpg'
 
 export default function Ubicacion() {
   return (
     <div className='div__ubicacion'>
-        
+        <div className='div__ubicacion_info'>
+          {/* <img className='img__ubicacion' src={test} alt="local taller mk" /> */}
+          <div className='div__imgagen_local' />
+        </div>
     </div>
   )
 }
