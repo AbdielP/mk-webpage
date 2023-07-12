30ee21eb443e8fc378c3c8e17072ed22a83b1ba7
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-12 02:39:28 -0500

    estilizadno mapa

--- src/components/Ubicacion.jsx
+++ src/components/Ubicacion.jsx
@@ -1,12 +1,19 @@
+import Button from '@mui/material/Button'
+import ArrowForwardSharpIcon from '@mui/icons-material/ArrowForwardSharp'
 import '../styles/ubicacion.css'
-import test from '../assets/images/local_foto.jpg'
 
 export default function Ubicacion() {
   return (
     <div className='div__ubicacion'>
         <div className='div__ubicacion_info'>
-          {/* <img className='img__ubicacion' src={test} alt="local taller mk" /> */}
           <div className='div__imgagen_local' />
+          <div className='div__info_ubicacion'>
+            <p>Av. Ernesto T. Lefevre</p>
+            <p>Al frente de la estación Delta</p>
+            <Button color='secondary' size='small' href='#' endIcon={<ArrowForwardSharpIcon /> } sx={{ color: '#FA5858',marginLeft: -.7 }}>
+            Ver ubicación
+            </Button>
+          </div>
         </div>
     </div>
   )
