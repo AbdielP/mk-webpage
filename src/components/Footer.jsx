e28879703db2f70d8198c323e6173aef47eccb8d
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-14 01:18:49 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook'
 import '../styles/footer.css'
 
 export default function Footer() {
   return (
     <footer className='footer'>
-       <h5 className='h5__footer'>TALLER <span className="">M</span><span className="span__k">K</span> ®</h5>
-       <div className='div__info_footer'>
-          <small>Mecánica general avanzada - Avenida Ernesto T. Lefevre, Frente a la estación Delta.</small>
-          <small>Teléfono: +507 6490-0247</small>
-       </div>
+      <div className='footer__left'>
+        <h4>TALLER <span>M</span><span className="span__k">K</span> ®</h4>
+        <div>
+          <small className="small__footer">Mecánica general avanzada</small>
+          <small className="small__footer">Avenida Ernesto T. Lefevre. Frente a la estación Delta</small>
+        </div>
+      </div>
+
+      <div className='footer__right'>
+        <div className='div__social'>
+          <InstagramIcon />
+          <FacebookIcon />
+          <small className='small__phone'><LocalPhoneIcon fontSize="inherit" sx={{marginRight: '.3em'}}/> +507 6490-0247</small>
+        </div>
+      </div>
     </footer>
   )
 }
