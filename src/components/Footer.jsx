0dbdfa37cad0ba182a7928f7779034e70da66c30
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-14 00:14:43 -0500

    footer basico

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,12 @@ import '../styles/footer.css'
 
 export default function Footer() {
   return (
-    <footer className='footer'>Footer</footer>
+    <footer className='footer'>
+       <h5 className='h5__footer'>TALLER <span className="">M</span><span className="span__k">K</span> ®</h5>
+       <div className='div__info_footer'>
+          <small>Mecánica general avanzada - Avenida Ernesto T. Lefevre, Frente a la estación Delta.</small>
+          <small>Teléfono: +507 6490-0247</small>
+       </div>
+    </footer>
   )
 }
