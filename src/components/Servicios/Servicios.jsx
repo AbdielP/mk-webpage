2340ebcbd780c2c2bf0c0e71447eaaa0b0cd3679
AbdielP <abdiel.pinzonc@gmail.com>
2023-07-30 16:03:25 -0500

    mejoras de espacio

--- src/components/Servicios/Servicios.jsx
+++ src/components/Servicios/Servicios.jsx
@@ -11,7 +11,7 @@ export default function Servicios() {
         <h1 className="h1">
           TALLER M<span className="span_rojo">K</span> - MECÁNICA AVANZADA
         </h1>
-        <h2 className="h2 span_rojo">Taller mecánico multimarca en Panamá. </h2>
+        <h2 className="h2 span_rojo">El mejor taller mecánico multimarca en Panamá. </h2>
         <p className="p__servicios" id="nuestros-servicios">
           Nuestro taller abarca una amplia gama de servicios, desde <b>mantenimiento
           preventivo</b>, <b>cambios de aceite</b>, <b>inspección y reemplazo de filtros</b>,
@@ -22,7 +22,7 @@ export default function Servicios() {
         <hr className="hr" />
 
         <p className="p__servicios">
-          Trabajamos con <span className="span_rojo">todas las marcas de vehiculos en Panamá</span>, como Toyota,
+          Trabajamos con <span className="span_rojo">todas las marcas de vehículos en Panamá</span>, como Toyota,
           Nissan, BMW, Hyundai, Kia, Audi, Porsche, Mercedez Benz y más...
         </p>
       </section>
